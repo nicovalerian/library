a4236fc4bf18b5c6c420eba38d3ce7b40b50d095
Nico <89244947+nicovalerian@users.noreply.github.com>
2024-05-07 15:19:17 +0700

    Add basic features

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const myLibrary = [];
 
-function Book() {
-
+function Book(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(title, author, pages) {
+    const newBook = new Book(title, author, pages, false);
+    myLibrary.push(newBook);
 }
